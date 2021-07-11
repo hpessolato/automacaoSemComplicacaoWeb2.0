c2329152f159be3e745db15b86d71441f82984cf
hpessolato <humbertopessolato@hotmail.com>
2021-07-11 17:39:18 -0300

    Aula 07

--- src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronosacademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,20 +12,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class TesteWeb {
-    @Test
-    public void primeiroTeste(){
+
+    WebDriver driver;
+
+    @Before
+    public void inicializaTeste(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
         driver.get("https://www.chronosacademy.com.br/");
-        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
-        WebElement h4Titulo = driver.findElement(By.xpath(xpathTitulo));
-        String titulo = h4Titulo.getText();
+
+    }
+
+    @Test
+    public void primeiroTeste(){
+
+        String xpathTitulo = "//section[2]//h4";
+
+        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
+        String titulo = txtTitulo.getText();
 
         assertEquals ("Porque Tempo É Conhecimento", titulo);
 
+        }
+
+    @Test
+    public void segundoTeste() {
+
+        String xpathBotao = "//section[2]/div[3]//a";
+        WebElement btnTitulo = driver.findElement(By.xpath(xpathBotao));
+        btnTitulo.click();
+        String xpathTitulo = "//section[2]//h2";
+        WebElement h2Titulo = driver.findElement(By.xpath(xpathTitulo));
+
+        assertEquals("AUTOMAÇÃO SEM COMPLICAÇÃO WEB 2.0", h2Titulo.getText());
+
+    }
+
+    @After
+    public void finalizaTeste(){
         driver.quit();
 
     }
