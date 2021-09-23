a9f8c3952e56fde76dcb5889448040bdeaf5a802
hpessolato <beto0403>
2021-09-22 23:30:50 -0300

    AULA 26

--- src/test/java/br/com/chronosacademy/runner/RunnerTest.java
+++ src/test/java/br/com/chronosacademy/runner/RunnerTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/",
         tags = "@alteracaoCRUD",
-        //altera
         glue = "br.com.chronosacademy.steps",
         plugin = {"json:target/reports/CucumberReports.json", "pretty"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
