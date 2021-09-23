a8598c7369de02aeff4fc67cb22a3dea9f13066a
hpessolato <beto0403>
2021-09-22 22:45:03 -0300

    aula 26 crud cadastro e login

--- src/test/java/br/com/chronosacademy/runner/RunnerTest.java
+++ src/test/java/br/com/chronosacademy/runner/RunnerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/",
         tags = "@alteracaoCRUD",
+        /altera
         glue = "br.com.chronosacademy.steps",
         plugin = {"json:target/reports/CucumberReports.json", "pretty"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
